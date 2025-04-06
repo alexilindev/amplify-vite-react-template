ee6cf89f7f42974818e70e64ec453a1cecb38b2d
Alex Ilin <alex.ilin.dev@gmail.com>
2025-04-06 20:47:30 +0200

    add scopes

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -10,7 +10,8 @@ export const auth = defineAuth({
     externalProviders: {
         google: {
           clientId: secret('GOOGLE_CLIENT_ID'),
-          clientSecret: secret('GOOGLE_CLIENT_SECRET')
+          clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+          scopes: ['email', 'profile', 'openid'],
         },
         callbackUrls: [
           'http://localhost:5173/',
